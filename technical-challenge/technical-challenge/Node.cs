dba4413d62e962f8663f50f2ff7633d0832ac0b9
jdroj <jdroj@JUAN_ROJAS>
2026-02-26 16:34:35 -0500

    challenge solution

---
+++ technical-challenge/technical-challenge/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace technical_challenge
+{
+    public class Node
+    {
+        public Node prev { get; set; }
+        public Node Next { get; set; }   
+        public int data { get; set; }
+
+        public Node(int data) 
+        { 
+            this.data = data;
+        }
+    }
+}
---
+++ technical-challenge/technical-challenge/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using technical_challenge;
+
+Console.WriteLine("Kommit technical-challenge");
+
+var list = new DoublyLinkedList();
+
+Console.WriteLine("Original List");
+
+list.InsertAtHead(10);
+list.InsertAtHead(22);
+list.InsertAtTail(31);
+list.InsertAtTail(42);
+list.InsertAtTail(43);
+list.InsertAtTail(31);
+
+
+list.PrintList();
+Console.WriteLine("Delete 22");
+list.DeleteNode(22);
+
+Console.WriteLine("List after remove");
+list.PrintList();
+
+Console.WriteLine("Delete duplicate 31");
+list.RemoveDuplicates();
+
+Console.WriteLine("List after remove duplicates");
+list.PrintList();
+
+
+var index = list.Search(43);
+// Considerando posiscion inicaial index 0
+Console.WriteLine($"Index de 22: {index}");
+
+list.CountNodes();
